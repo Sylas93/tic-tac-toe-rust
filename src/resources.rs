6591a62d71ceb841100968a1ad9b2968f534674c
brack <angelo.gaming.93@outlook.it>
2025-05-22 16:52:26 +0200

    feat: read resources async

--- src/resources.rs
+++ src/resources.rs
@@ -1,14 +1,16 @@
-use std::fs;
+use tokio::fs;
 use tokio::sync::OnceCell;
 
-fn read_resource(resource_path: &str) -> Vec<u8> {
-    fs::read(resource_path).expect("Could not load resources")
+async fn read_resource(resource_path: &str) -> Vec<u8> {
+    fs::read(resource_path).await
+        .expect("Could not load resources")
 }
 
-fn read_js_with_endpoint(ws_endpoint: &str) -> Vec<u8> {
-    let javascript = fs::read_to_string("src/static/app.js")
-        .expect("Could not load javascript");
-    javascript.replace("SOCKET_HOST", ws_endpoint).into_bytes()
+async fn read_js_with_endpoint(ws_endpoint: &str) -> Vec<u8> {
+    let javascript = fs::read_to_string("src/static/app.js").await;
+        javascript
+        .expect("Could not load javascript")
+        .replace("SOCKET_HOST", ws_endpoint).into_bytes()
 }
 
 pub struct StaticResource {
@@ -24,14 +26,23 @@ pub struct StaticResource {
 impl StaticResource {
     pub async fn new(ws_endpoint: &str) -> &'static StaticResource {
         STATIC_RESOURCE.get_or_init(|| async {
+            let resources = tokio::join!(
+                read_resource("src/static/index.html"),
+                read_js_with_endpoint(ws_endpoint),
+                read_resource("src/static/images/empty-cell.jpg"),
+                read_resource("src/static/images/x-cell.jpg"),
+                read_resource("src/static/images/o-cell.jpg"),
+                read_resource("src/static/grid.css"),
+                read_resource("src/static/images/favicon.png"),
+            );
             StaticResource {
-                homepage: read_resource("src/static/index.html"),
-                javascript: read_js_with_endpoint(ws_endpoint),
-                empty_cell: read_resource("src/static/images/empty-cell.jpg"),
-                x_cell: read_resource("src/static/images/x-cell.jpg"),
-                o_cell: read_resource("src/static/images/o-cell.jpg"),
-                css: read_resource("src/static/grid.css"),
-                favicon: read_resource("src/static/images/favicon.png"),
+                homepage: resources.0,
+                javascript: resources.1,
+                empty_cell: resources.2,
+                x_cell: resources.3,
+                o_cell: resources.4,
+                css: resources.5,
+                favicon: resources.6,
             }
         }).await
     }
